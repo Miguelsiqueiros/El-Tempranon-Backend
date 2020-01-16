0d374a85176d659505c6e96b5e782fd82626f290
Jorge Antonio Perez Nevarez <antoniopnevarez@gmail.com>
2020-01-15 16:21:21 -0800

    Checkin with validations

--- src/API/users/checkin.js
+++ src/API/users/checkin.js
@@ -1,69 +1,50 @@
 const mongoClient = require('../../data/mongo-server');
 const logger = require('../../log/logger');
 const checkinSO = require('../../data/schema/checkin-schema');
-const url = 'mongodb://localhost:27017/Tempranon';
-//const MongoClientTest = require('mongodb').MongoClient;
 
 module.exports = function(req, res) {
     const pin = req.body.pin
-    if(pin === undefined || pin === null){
-        res.status(404).send('PIN not found in request')
+    if(pin === undefined || pin === null || pin.toString().length !== 4){
+        let resp = {status: `PIN ${pin} is invalid.`}
+        res.status(400).send(JSON.stringify(resp))
     }else{
-        const date = new Date();
-        if (validateCheckin(pin, date) === 0){
-            const firstDateOfYear = new Date(date.getFullYear(), 0, 1);
-            const pastDaysOfYear = (date - firstDateOfYear) / 86400000;
-            const weekNumber = Math.ceil((pastDaysOfYear + firstDateOfYear.getDay() + 1) / 7);
-            const minutesToday = ((date.getHours()-8) * 60) + date.getMinutes();
-            const currentDate = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
-            const check = new checkinSO({pin: pin, pto: false, date: currentDate, minutes: minutesToday, week: weekNumber});
-            /*if(checkin(check)){
-                res.status(200).send('Checked successfully')
-            }else{
-                res.status(500).send('Something went wrong')
-            }*/
-            res.status(200).send('Checked successfully')
-        }else if (validateCheckin(pin, date) === 1){
-            res.status(400).send('Invalid PIN')
-        }else{
-            res.status(400).send('User already checked today')
-        }
-    }
-}
+        mongoClient.then(client => {
+            const date = new Date();
+            const collectionCheckin = client.db('Tempranon').collection('checkin');
+            const collectionUsers = client.db('Tempranon').collection('users');
 
-function validateCheckin(pin, date){
-    if(pin.toString().length !== 4){
-        return 1;
-    }else{
-        MongoClientTest.connect(url, function(err, db){
-            if(err) return 2;
-            const dbo = db.db("Tempranon");
-            dbo.collection("checkin").find({pin : pin, date : {$regex : date.toLocaleDateString()}})
-            .toArray(function(err, res){
-                if(err) return logger.warn(err);
-                if(res.length > 0){
-                    console.log("ya checo hoy")
-                    return 1;
+            collectionUsers.find({pin:pin}).toArray(function (err, items){
+                if(err) logger.warn(err)
+                if(items.length <= 0){
+                    let resp = {status: `Cannot found user with PIN ${pin}`}
+                    res.status(204).send(JSON.stringify(resp));
                 }else{
-                    console.log("ve a checar")
-                    return 0;
+                    collectionCheckin.find({pin : pin, date : {$regex : date.toLocaleDateString()}}).toArray(function (err, item) {
+                        if(err) logger.warn(err)
+                        if(item.length > 0){
+                            let resp = {status: `User with PIN ${pin} already checked today`}
+                            res.status(200).send(JSON.stringify(resp));
+                        }else{
+                            try{
+                                const firstDateOfYear = new Date(date.getFullYear(), 0, 1);
+                                const pastDaysOfYear = (date - firstDateOfYear) / 86400000;
+                                const weekNumber = Math.ceil((pastDaysOfYear + firstDateOfYear.getDay() + 1) / 7);
+                                const minutesToday = ((date.getHours()-8) * 60) + date.getMinutes();
+                                const currentDate = `${date.toLocaleDateString()} ${date.toLocaleTimeString()}`;
+                                const check = new checkinSO({pin: pin, pto: false, date: currentDate, minutes: minutesToday, week: weekNumber});
+                    
+                                collectionCheckin.insertOne(check, function(err, result){
+                                    if(err) logger.warn(err)
+                                });
+                            }catch(execptionError){
+                                logger.warn(execptionError)
+                            }
+                            let resp = {status: `Checked successfull`, date : `${currentDate}`}
+                            res.status(201).send(JSON.stringify(resp));
+                        }
+                    });
                 }
-                
-                db.close();
             });
         });
     }
-    return 0;
 }
-
-/*function checkin(check){
-    MongoClientTest.connect(url, function(err, db) {
-        if (err) return false;
-        var dbo = db.db("Tempranon");
-        dbo.collection("checkin").insertOne(check, function(err, res) {
-          if (err) logger.warn(err);
-          db.close();
-        });
-    });
-    return true;
-}*/
