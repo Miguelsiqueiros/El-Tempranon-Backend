9729e3c3c084dc15fce55f6b4195590b426a67ef
Jorge Antonio Perez Nevarez <antoniopnevarez@gmail.com>
2020-02-03 13:36:30 -0800

    Update register, checkin and weekly ranking and refactor validations

--- src/API/users/register.js
+++ src/API/users/register.js
@@ -1,24 +1,33 @@
-const logger = require('../../log/logger');
-const users = require('../../data/schema/user-schema');
-const { validationResult } = require('express-validator');
+const logger = require("../../log/logger");
+const users = require("../../data/schema/user-schema");
+const { validationResult } = require("express-validator");
 
-module.exports = function(req, res) {
+module.exports = async function(req, res) {
   const { name, image, email } = req.body;
   const errores = validationResult(req);
   if (!errores.isEmpty())
     return res.status(422).json({ errores: errores.array() });
 
+  if (await users.findOne({ email: email }))
+    return res.status(409).json({ errores: "Email already in use." });
+
   SelectRandomNumber().then(newPin => {
-    const document = { pin: newPin, name: name, image: image, email: email };
-    users.create(document, (error, doc) => {
+    const user = new users({
+      pin: newPin,
+      name: name,
+      image: image,
+      email: email
+    });
+
+    users.create(user, (error, doc) => {
       if (error) logger.warn(error.message);
-      res.status(201).json(document);
+      res.status(201).json(user);
     });
   });
 };
 
 async function SelectRandomNumber() {
-  let randomPin = Math.round(Math.random() * 9999);
+  let randomPin = Math.round(Math.random() * (9999 - 1000) + 1000);
   let foundPin = await users.findOne({ pin: randomPin }, (error, doc) => {
     if (error) {
       logger.warn(error.message);
--- test/test-register.js
+++ test/test-register.js
@@ -1,32 +1,25 @@
-const assert = require('assert');
+const assert = require("assert");
 
-const request = require('supertest');
-const app = require('../server');
-const mongodbMemoryServer = require('./util/mongodb-in-memory');
+const request = require("supertest");
+const app = require("../server");
+const mongodbMemoryServer = require("./util/mongodb-in-memory");
 
-const proxyRequire = require('proxyquire');
+const userSchema = require("../src/data/schema/user-schema");
+const faker = require("faker");
 
-const userSchema = require('../src/data/schema/user-schema');
-
-const mongoClientStub = {};
-const mongodbMock = proxyRequire('../server', {
-  './src/data/mongo-server': mongoClientStub
-});
-const faker = require('faker');
-
-describe('Test register endpoint', () => {
+describe("Test register endpoint", () => {
   before(async () => {
-    mongoClientStub.call = await mongodbMemoryServer.Connect();
+    await mongodbMemoryServer.Connect();
     mongodbMemoryServer.clearDatabase();
   });
-  it('/POST should create a user when a name and email is provided', done => {
+  it("/POST should create a user when a name and email is provided", done => {
     request(app.call())
-      .post('/api/v1/users/create')
+      .post("/api/v1/users/create")
       .send({
         name: faker.name.findName(),
         email: `${faker.name.firstName().trim()}@email.com`
       })
-      .set('Accept', 'application/json')
+      .set("Accept", "application/json")
       .end((err, result) => {
         const pin = result.body.pin;
         assert.equal(result.status, 201);
@@ -36,11 +29,18 @@ describe('Test register endpoint', () => {
         done();
       });
   });
-  it('/POST should return bad request when name is not provided', done => {
+  it("/POST should return bad request when name is not provided", done => {
     request(app.call())
-      .post('/api/v1/users/create')
+      .post("/api/v1/users/create")
       .send({ email: `${faker.name.firstName().trim()}@email.com` })
-      .set('Accept', 'application/json')
+      .set("Accept", "application/json")
+      .expect(422, done);
+  });
+  it("/POST should return bad request when email is not provided", done => {
+    request(app.call())
+      .post("/api/v1/users/create")
+      .send({ name: `${faker.name.findName()}` })
+      .set("Accept", "application/json")
       .expect(422, done);
   });
 });
