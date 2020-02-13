9b042b99e64316f50172ec3d3d8bb6a4ce12a31a
Jorge Antonio Perez Nevarez <antoniopnevarez@gmail.com>
2020-02-13 11:20:32 -0800

    Update name, arrival hour, and image for a user, and add one validation for register

---
+++ src/API/users/update-data.js
@@ -0,0 +1,40 @@
+const logger = require("../../log/logger");
+const users = require("../../data/schema/user-schema");
+const { validationResult } = require("express-validator");
+
+module.exports = function(req, res) {
+  const errors = validationResult(req);
+  if (!errors.isEmpty())
+    return res.status(422).json({ errors: errors.array() });
+
+  const { pin, arrival, name, image } = req.body;
+
+  users.update(
+    { pin: pin },
+    {
+      $set: {
+        arrival: arrival,
+        name: name,
+        image: image
+      }
+    },
+    (err, item) => {
+      if (err) {
+        logger.warn(err);
+      }
+      if (item) {
+        let resp = {
+          info: `User information updated succesful.`,
+          type: "success"
+        };
+        res.status(200).send(JSON.stringify(resp));
+      } else {
+        let resp = {
+          info: `Failed updating the information for user with PIN: ${pin}`,
+          type: "error"
+        };
+        res.status(409).send(JSON.stringify(resp));
+      }
+    }
+  );
+};
--- src/API/validators/updateUser-validator.js
+++ src/API/validators/updateUser-validator.js
@@ -1,16 +1,17 @@
 const { check } = require("express-validator");
 
 module.exports = [
+  check("pin")
+    .isNumeric()
+    .isLength({ min: 4, max: 4 })
+    .withMessage("Please enter a valid PIN")
+    .notEmpty()
+    .withMessage("Field pin is required"),
   check("name")
     .isString()
-    .withMessage("Name should only have letters")
+    .withMessage("Name should only have letters.")
     .notEmpty()
     .withMessage("Field name is required"),
-  check("email")
-    .isEmail()
-    .withMessage("Email format is incorrect")
-    .notEmpty()
-    .withMessage("Field email is required"),
   check("arrival")
     .notEmpty()
     .withMessage("Field arrival is required")
